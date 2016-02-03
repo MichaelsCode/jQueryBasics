1c005b749123187a14d90a133798425dbd09fe1e
MichaelsCode <michael.hernandez.solorzano@gmail.com>
2016-02-03 02:04:02 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password")
+$("form span").hide();
+
+function isPasswordValid(){
+	return $password.val().length > 8;
+}
+
+function arePasswordsMatching(){
+	return $password.val() === $confirmPassword.val();
+}
+
+function passwordEvent(){
+	if(isPasswordValid()){
+		$password.next().hide();
+	}else{
+		$password.next().show();
+	}
+}
+
+function confirmPasswordEvent(){
+	if(arePasswordsMatching()){
+		$confirmPassword.next().hide();
+	}else{
+		$confirmPassword.next().show();
+	}
+
+}
+
+
+$password.focus(passwordEvent).keyup(passwordEvent)
+	.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+
